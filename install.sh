ed2b955d696da6b2b0cd108484ec0a14a8a46956
shintran <fctranngoc@gmail.com>
2025-01-19 16:42:52 +0700

    Initial commit of dotfiles

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Tạo symlink cho các tệp cấu hình
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+# Thêm các symlink khác nếu cần
