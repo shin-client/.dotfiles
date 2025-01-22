9962530d039aecf2d882e3e8e23dda22e0e99d99
shin-client <fctranngoc@gmail.com>
2025-01-22 20:11:03 +0700

    update somethings

--- nvim/.config/nvim/lua/shintran/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/shintran/plugins/whichkey.lua
@@ -1,6 +1,17 @@
 return {
 	"folke/which-key.nvim",
+	event = "VeryLazy",
 	config = function()
-		require("which-key").setup({})
+		local wk = require("which-key")
+		wk.add({
+			{
+				"<leader>w",
+				desc = "Save file",
+			},
+			{
+				"<leader>q",
+				desc = "Quit",
+			},
+		})
 	end,
 }
